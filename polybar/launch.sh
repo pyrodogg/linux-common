7ca50d3ecc1f1246bb947c250fe29541b87c6879
Umut E <amiculus.umut@hotmail.de>
2019-02-05 11:16:45 +0100

    add configs

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar dp0 &
+polybar dp4 &
+echo "Polybar launched..."
